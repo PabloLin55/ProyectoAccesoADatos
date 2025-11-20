401d92af7262b7ee7f8c76f7255c1e4bc3a1fdbf
pablol55 <pablo.lin@educa.madrid.org>
2025-11-20 12:14:53 +0100

    Cambios en el main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,19 @@
 package org.example;
 
-import DAO.LibroDAO;
-import DAO.LibroDAOImpl;
+import DAO.*;
 
 public class Main {
     public static void main(String[] args) {
         LibroDAO libroDAO = new LibroDAOImpl();
+        AutorDAO autorDAO = new AutorDAOImpl();
+        PrestamoDAO prestamoDAO = new PrestamoDAOImpl();
+        UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
+
 
         LibrosService servicioLibros = new LibrosService(libroDAO);
         AutorService servicioAutores = new AutorService(autorDAO);
         PrestamoService servicioPrestamos = new PrestamoService(prestamoDAO);
-        UsuarioService servicioAutores = new UsuarioService(UsuarioDAO);
+        UsuarioService servicioAutores = new UsuarioService(usuarioDAO);
 
     }
 }
